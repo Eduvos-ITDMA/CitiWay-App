d07440ccf5cfc287c99d00a54bcefcae92f81abc
Makanaka Mamutse <makanakamamutse@gmail.com>
2025-10-03 21:47:22 +0200

    Fix: Updated ScreenWrapper, removed drawerState

--- app/src/main/java/com/example/citiway/features/schedules/SchedulesRoute.kt
+++ app/src/main/java/com/example/citiway/features/schedules/SchedulesRoute.kt
@@ -1,19 +1,16 @@
 package com.example.citiway.features.schedules
 
 import androidx.compose.runtime.Composable
-
-
 import androidx.navigation.NavController
 import com.example.citiway.core.util.ScreenWrapper
-
 
 @Composable
 fun SchedulesRoute(
     navController: NavController,
 ) {
-    ScreenWrapper(navController, drawerState, true) {paddingValues ->
-         SchedulesContent(
+    ScreenWrapper(navController, true, { paddingValues ->
+        SchedulesContent(
             paddingValues = paddingValues,
         )
-    }
+    })
 }
