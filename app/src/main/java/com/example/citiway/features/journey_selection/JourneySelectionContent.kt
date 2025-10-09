36e03130cb64b283ab7fca6faffc77aa8370272d
Caleb Riordan <caleb2003riordan@gmail.com>
2025-10-09 14:26:03 +0200

    Fix fusedLocationClient.lastLocation null error

--- app/src/main/java/com/example/citiway/features/journey_selection/JourneySelectionContent.kt
+++ app/src/main/java/com/example/citiway/features/journey_selection/JourneySelectionContent.kt
@@ -1,8 +1,5 @@
 package com.example.citiway.features.journey_selection
 
-
-// HomeContent.kt
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -12,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
@@ -20,7 +16,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +27,6 @@ import com.example.citiway.core.utils.JourneySelectionScreenPreview
 import com.example.citiway.data.remote.PlacesActions
 import com.example.citiway.data.remote.PlacesState
 import com.example.citiway.features.shared.JourneyState
-import com.google.android.libraries.places.api.model.AutocompletePrediction
 
 @Composable
 fun JourneySelectionContent(
